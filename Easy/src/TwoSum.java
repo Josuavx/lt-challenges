7bc40d0d6498fc06ade48b1d4b4fe437bc826017
Josué <josueavx@gmail.com>
2023-03-10 18:03:57 -0300

    first challenge

--- Easy/src/TwoSum.java
+++ Easy/src/TwoSum.java
@@ -1,6 +1,9 @@
+//Leetcode challenge link: https://leetcode.com/problems/two-sum/description/
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int sum[] = new int [2];
+
         for (int i = 0; i < nums.length - 1; i++){
             for (int j = i + 1; j < nums.length; j++){
                 if (nums[i] + nums[j] == target) {
